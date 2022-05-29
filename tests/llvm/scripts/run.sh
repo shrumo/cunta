659fd9e198a873e78270651c733dfd1fc0a4c1d5
Maksymilian Mika <mika.maksymilian@gmail.com>
2022-05-29 16:47:18 +0100

    Getting rid of the llvm test.

--- tests/llvm/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e # Exit if any command is non zero return value
-
-PROJECT_SOURCE_DIR="$(pwd)/${0%/*}/.."
-cd ${PROJECT_SOURCE_DIR}
-
-cd build
-echo "def foo (x y) x+foo(y, 4.0);" | ./run
